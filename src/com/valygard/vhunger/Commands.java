bb32391ecbb5903390d7c33526effebe1bd9ce8b
AoHRuthless <kumara0398@gmail.com>
2014-03-28 17:22:19 -0400

    Added Commands.

---
+++ src/com/valygard/vhunger/Commands.java
@@ -0,0 +1,223 @@
+/**
+ * Commands.java is a part of the plugin vHunger.
+ *
+ * Copyright (C) 2013 Anand Kumar <http://dev.bukkit.org/bukkit-plugins/vhunger/>
+ *
+ * vHunger is a free software: You can redistribute it or modify it
+ * under the terms of the GNU General Public License published by the Free
+ * Software Foundation, either version 3 of the license of any later version.
+ * 
+ * vHunger is distributed in the intent of being useful. However, there
+ * is NO WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You can view a copy of the GNU General Public License at 
+ * <http://www.gnu.org/licenses/> if you have not received a copy.
+ */
+package com.valygard.vhunger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * @author Anand
+ *
+ */
+public class Commands implements CommandExecutor {
+
+	private Hunger plugin;
+	
+	public Commands(Hunger plugin) {
+		this.plugin = plugin;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (!(sender instanceof Player))
+			return false;
+		
+		Player p = (Player) sender;
+		int hunger = p.getFoodLevel();
+		
+		if (!cmd.getName().equalsIgnoreCase("hunger"))
+			return false;
+		
+		if (args.length == 0) {
+			if (!p.hasPermission("hunger.check")) {
+				sendIncorrectMessage(p, false, true);
+				return false;
+			}
+			tell(p, "Your hunger is &e" + hunger + ".");
+		}
+		
+		else if (args.length == 1) {
+			if (args[0].equalsIgnoreCase("help")) {
+				tell(p, "&2------ [&avHunger (Version " + plugin.getDescription().getVersion() + ")&2] ------");
+				tell(p, "&a/hunger [player] &r-&e Check a player's hunger.");
+				if(p.hasPermission("hunger.admin")) {
+					tell(p, "&a/hunger nourish [player] &r-&e Feed a player.");
+					tell(p, "&a/hunger set [player] <integer> &r-&e Set your own hunger or another player's.");
+					tell(p, "&a/hunger config <save|reload> &r-&e Save or reload the config file.");
+				}	
+			}
+			
+			else if (args[0].equalsIgnoreCase("nourish")) {
+				if (!p.hasPermission("hunger.nourish")) {
+					sendIncorrectMessage(p, false, true);
+					return false;
+				}
+				
+				// Set their hunger and saturation to the max.
+				p.setFoodLevel(20);
+				p.setSaturation(20);
+				
+				tell(p, "You have been satiated.");
+			} else {
+				// If the args[0] is anything else, it should check if its a player.
+				if (!p.hasPermission("hunger.check.others")) {
+					sendIncorrectMessage(p, false, true);
+					return false;
+				}
+				
+				// Check is player is null.
+				Player args1 = Bukkit.getPlayer(args[0]);
+				if (args1 == null) {
+					tell(p, "The player &e " + args[0] + "&r is offline or does not exist.");
+					return false;
+				}
+				
+				tell(p, args1 + "'s hunger level is &e" + args1.getFoodLevel() + ".");
+				
+			}
+		}
+		
+		else if (args.length == 2) {
+			if (args[0].equalsIgnoreCase("config")) {
+				if (args[1].equalsIgnoreCase("reload")) {
+					if (!p.hasPermission("hunger.config.reload")) {
+						sendIncorrectMessage(p, false, true);
+						return false;
+					}
+					
+					// Reload config
+					plugin.reloadConfig();
+					tell(p, "Config successfully reloaded.");
+				}
+				
+				else if (args[1].equalsIgnoreCase("save")) {
+					if (!p.hasPermission("hunger.config.save")) {
+						sendIncorrectMessage(p, false, true);
+						return false;
+					}
+					
+					// Save Config
+					plugin.saveConfig();
+					tell(p, "Config successfully saved.");
+				}
+				
+				else {
+					sendIncorrectMessage(p, false, true);
+					return false;
+				}
+			} 
+			
+			else if (args[0].equalsIgnoreCase("set")) {
+				if (!p.hasPermission("hunger.set")) {
+					sendIncorrectMessage(p, false, true);
+					return false;
+				}
+				
+				try {
+					int newHunger = Integer.parseInt(args[1]);
+					p.setFoodLevel(newHunger);
+					p.setSaturation(newHunger);
+					tell(p, "You have set your hunger to &e" + newHunger + ".");
+				} catch (NumberFormatException e) {
+					throw new NumberFormatException("Expected integer between 0-20 for hunger!");
+				}
+			}
+			
+			else if (args[0].equalsIgnoreCase("nourish")) {
+				if (!p.hasPermission("hunger.nourish.others")) {
+					sendIncorrectMessage(p, false, true);
+					return false;
+				}
+				
+				Player args2 = Bukkit.getPlayer(args[1]);
+				
+				if (args2 == null) {
+					tell(p, "The player &e " + args[1] + "&r is offline or does not exist.");
+					return false;
+				}
+				
+				// Feed the player.
+				args2.setFoodLevel(20);
+				args2.setSaturation(20);
+				
+				tell(p, "You have satiated &e " + args2.getName() + ".");
+				tell(args2, "You have been satiated.");
+			}
+			
+			else {
+				sendIncorrectMessage(p, true, false);
+				return false;
+			}
+		}
+		
+		else if (args.length > 3) {
+			// Our only command with 3 args is /hunger set...
+			if (!args[0].equalsIgnoreCase("set")) {
+				sendIncorrectMessage(p, true, false);
+				return false;
+			}
+			
+			if (!p.hasPermission("hunger.set.others")) {
+				sendIncorrectMessage(p, false, true);
+				return false;
+			}
+			
+			Player args2 = Bukkit.getPlayer(args[1]);
+			
+			if (args2 == null) {
+				tell(p, "The player &e " + args[1] + "&r is offline or does not exist.");
+				return false;
+			}
+			
+			try {
+				int newHunger = Integer.parseInt(args[2]);
+				
+				args2.setFoodLevel(newHunger);
+				args2.setSaturation(newHunger);
+				
+				tell(args2, "Your hunger is now &e " + newHunger + ".");
+				tell(p, "You have set the hunger of &e " + args2.getName() + " &rto&e " + newHunger + ".");
+			} catch (NumberFormatException e) {
+				throw new NumberFormatException("Expected integer between 0-20 for hunger!");
+			}
+		}
+		return true;
+	}
+	
+	// Send a message with prefix and allow for '&' hex-color codes.
+	private void tell(Player p, String msg) {
+		p.sendMessage(ChatColor.translateAlternateColorCodes('&', ChatColor.GREEN + "[vHunger] " + ChatColor.RESET + msg));
+	}
+	
+	// Our incorrect usage formulator.
+	private void sendIncorrectMessage(Player p, boolean hasPermission, boolean invalidArgs) {
+		if (!hasPermission)
+			tell(p, "You do not have permission to use this command.");
+		if (!invalidArgs)
+			tell(p, "Invalid arguments. Use &e/hunger help&r for a list of commands.");
+	}
+	
+	public Hunger getPlugin() {
+		return plugin;
+	}
+
+}
